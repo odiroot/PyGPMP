ecaf9418bfbaa1dff136c11ada491d428ceb00ef
odiroot <odi.root@gmail.com>
2012-07-12 14:10:16 +0200

    Email/password controls for login window.

--- gpmp/ui/account_login.py
+++ gpmp/ui/account_login.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'gpmp/ui/account_login.ui'
 #
-# Created: Sat Jul  7 00:37:09 2012
+# Created: Thu Jul 12 13:40:47 2012
 #      by: PyQt4 UI code generator 4.9.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -21,11 +21,30 @@ class Ui_AccountLogin(object):
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
         self.verticalLayout = QtGui.QVBoxLayout(self.centralwidget)
         self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
-        self.label = QtGui.QLabel(self.centralwidget)
-        self.label.setAlignment(QtCore.Qt.AlignCenter)
-        self.label.setWordWrap(True)
-        self.label.setObjectName(_fromUtf8("label"))
-        self.verticalLayout.addWidget(self.label)
+        self.lbl_info = QtGui.QLabel(self.centralwidget)
+        self.lbl_info.setAlignment(QtCore.Qt.AlignCenter)
+        self.lbl_info.setWordWrap(True)
+        self.lbl_info.setObjectName(_fromUtf8("lbl_info"))
+        self.verticalLayout.addWidget(self.lbl_info)
+        self.gridLayout = QtGui.QGridLayout()
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.edt_email = QtGui.QLineEdit(self.centralwidget)
+        self.edt_email.setObjectName(_fromUtf8("edt_email"))
+        self.gridLayout.addWidget(self.edt_email, 0, 1, 1, 1)
+        self.lbl_email = QtGui.QLabel(self.centralwidget)
+        self.lbl_email.setObjectName(_fromUtf8("lbl_email"))
+        self.gridLayout.addWidget(self.lbl_email, 0, 0, 1, 1)
+        self.lbl_password = QtGui.QLabel(self.centralwidget)
+        self.lbl_password.setObjectName(_fromUtf8("lbl_password"))
+        self.gridLayout.addWidget(self.lbl_password, 1, 0, 1, 1)
+        self.edt_password = QtGui.QLineEdit(self.centralwidget)
+        self.edt_password.setEchoMode(QtGui.QLineEdit.Password)
+        self.edt_password.setObjectName(_fromUtf8("edt_password"))
+        self.gridLayout.addWidget(self.edt_password, 1, 1, 1, 1)
+        self.verticalLayout.addLayout(self.gridLayout)
+        self.btn_login = QtGui.QPushButton(self.centralwidget)
+        self.btn_login.setObjectName(_fromUtf8("btn_login"))
+        self.verticalLayout.addWidget(self.btn_login)
         AccountLogin.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(AccountLogin)
@@ -33,5 +52,8 @@ class Ui_AccountLogin(object):
 
     def retranslateUi(self, AccountLogin):
         AccountLogin.setWindowTitle(QtGui.QApplication.translate("AccountLogin", "Log in to Google Music", None, QtGui.QApplication.UnicodeUTF8))
-        self.label.setText(QtGui.QApplication.translate("AccountLogin", "<html><head/><body><p>In order to use Google Music you have to specify your Google account\'s email address and password.<br/>You only need to do this once.</p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+        self.lbl_info.setText(QtGui.QApplication.translate("AccountLogin", "<html><head/><body><p>In order to use Google Music you have to specify your Google account\'s email address and password.<br/>You only need to do this once.</p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+        self.lbl_email.setText(QtGui.QApplication.translate("AccountLogin", "Email", None, QtGui.QApplication.UnicodeUTF8))
+        self.lbl_password.setText(QtGui.QApplication.translate("AccountLogin", "Password", None, QtGui.QApplication.UnicodeUTF8))
+        self.btn_login.setText(QtGui.QApplication.translate("AccountLogin", "Log in", None, QtGui.QApplication.UnicodeUTF8))
 
--- gpmp/windows.py
+++ gpmp/windows.py
@@ -3,30 +3,43 @@ from PyQt4.QtCore import QCoreApplication, Qt
 from PyQt4.QtGui import QMainWindow
 
 from gpmp.ui.account_login import Ui_AccountLogin
+from gpmp.model import Model
 
 
 class MainWindow(QMainWindow):
     u"""The main window after app is launched."""
     def __init__(self, parent=None):
         QMainWindow.__init__(self, parent)
-        # TODO: Model like in hsoft/guiskel
         self.setWindowTitle(QCoreApplication.instance().applicationName())
+        self.model = Model()
+
         # TODO: MainWindow ui/pyui.
         try:
             self.setAttribute(Qt.WA_Maemo5StackedWindow)
         except:
             pass
+        # Show main window and proceed with usual behaviour.
         self.show()
+        self.run()
 
-        self.login_window = LoginWindow(self)
-        self.login_window.show()
+    def run(self):
+        if not self.model.logged_in:
+            self.handle_login()
+
+    def handle_login(self):
+        login_window = LoginWindow(self, model=self.model)
+        login_window.show()
 
 
 class LoginWindow(QMainWindow, Ui_AccountLogin):
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, model=None):
         QMainWindow.__init__(self, parent)
+        self.model = model
         try:
             self.setAttribute(Qt.WA_Maemo5StackedWindow)
         except:
             pass
         self.setupUi(self)
+
+    # def closeEvent(self, event):
+    #     return QMainWindow.closeEvent(self, event)
