d49f214d5d0c7bf9cc97f3ab6986873889ba397b
odiroot <odi.root@gmail.com>
2013-01-06 16:14:11 +0100

    Fixed stdeb dependencies. Added desktop file.

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ config = {
     "description": "Unofficial Google Play Music player",
     "author": "Michal Odnous",
     "url": "https://github.com/odiroot/PyGPMP",
-    "version": "0.1",
+    "version": "0.2",
     "license": "BSD",
     "install_requires": ["gmusicapi"],
-    "packages": ["gpmp"],
+    "packages": ["gpmp", "gpmp.ui"],
     "scripts": [],
     "entry_points": {
         "console_scripts": ["pygpmp = gpmp.run:main"]
